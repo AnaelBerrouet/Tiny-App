26f7c76280036b1d2dc58f37d9c755adf72b5e6d
AnaelBerrouet <anael.jberrouet@gmail.com>
2018-10-04 18:40:59 +0000

    added user authentication based on password storage and unique user IDs stored in cookies

--- express-server.js
+++ express-server.js
@@ -1,3 +1,7 @@
+//#############################################
+//~~~~~~~~~~~~~~~~~~ SETUP - REQUIRES ~~~~~~~~~~~~~~~~~~~~~
+//#############################################
+
 //load in express
 let express = require('express');
 let app = express();
@@ -7,12 +11,20 @@ var cookieParser = require('cookie-parser')
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
+//#############################################
+//~~~~~~~~~~~~~~~~~~ MIDDLEWARE ~~~~~~~~~~~~~~~~~~~~~
+//#############################################
+
 app.use(cookieParser());
 
 //Add body parser to handle POST requests
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended:true}));
 
+
+//#############################################
+//~~~~~~~~~~~~~~~~~~ DB / HARDCODES ~~~~~~~~~~~~~~~~~~~~~
+//#############################################
 //Select port
 const PORT = 8080;
 
@@ -20,6 +32,23 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+const users = {
+  "userRandomID": {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+ "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  },
+};
+
+//#############################################
+//~~~~~~~~~~~~~~~~~~ UTILITY FUNCTIONS ~~~~~~~~~~~~~~~~~~~~~
+//#############################################
 
 //Returns a random string of random length under 10
 function generateRandomString() {
@@ -32,28 +61,56 @@ function generateRandomString() {
   return str;
 }
 
+// returns array of objects that contain certain attribute "attrib"
+function filterByAttribute(filteredObj, attrib, value) {
+  let fileredObjArray = [];
+  for(let obj in filteredObj) {
+    if(attrib in filteredObj[obj]){
+      fileredObjArray.push(filteredObj[obj]);
+    }
+  }
+  return fileredObjArray.filter((user) => { return user[attrib] == value});
+}
+
+// console.log(filterByAttribute(users, "email", "user@example.com"));
+
+//#############################################
+//~~~~~~~~~~~~~~~~~~ GET ROUTES ~~~~~~~~~~~~~~~~~~~~~
+//#############################################
 
 //DEFINE SERVER FUNCTIONALITY (ROUTES ETC)
 //Home page
 app.get("/", (req, res) => {
-  res.render('pages/index',{username: req.cookies["username"]});
+  res.render('pages/index',{user: users[req.cookies["user_id"]]});
+});
+
+//GET /register endpoint, which returns a page that includes a form with an email and password field.
+//The email field should use type=email and have name=email. The password field should use type=password and have name=password.
+// registration page
+app.get('/register', function(req, res) {
+    res.render('pages/register',{user: users[req.cookies["user_id"]]});
+});
+
+// login page
+app.get('/login', function(req, res) {
+    res.render('pages/login',{user: users[req.cookies["user_id"]]});
 });
 
 // about page
 app.get('/about', function(req, res) {
-    res.render('pages/about', {username: req.cookies["username"]});
+    res.render('pages/about', {user: users[req.cookies["user_id"]]});
 });
 
 //URLs page
 app.get("/urls", (req, res) => {
   res.render('pages/urls_index', {urls: urlDatabase,
-    username: req.cookies["username"]
+    user: users[req.cookies["user_id"]]
   });
 });
 
 //create new URL page
 app.get("/urls/new", (req, res) => {
-  res.render('pages/urls_new', {username: req.cookies["username"]});
+  res.render('pages/urls_new', {user: users[req.cookies["user_id"]]});
 });
 
 //Specific URL page
@@ -61,7 +118,7 @@ app.get("/urls/:id", (req, res) => {
   res.render('pages/urls_show', {
     shortUrl: req.params.id,
     longUrl: urlDatabase[req.params.id],
-    username: req.cookies["username"]
+    user: users[req.cookies["user_id"]]
   });
 });
 
@@ -70,6 +127,11 @@ app.get("/u/:shortURL", (req, res) => {
   let longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
 });
+
+
+//#############################################
+//~~~~~~~~~~~~~~~~~~ POST ROUTES ~~~~~~~~~~~~~~~~~~~~~
+//#############################################
 
 //Add URL to database
 app.post('/urls', (req, res) => {
@@ -81,25 +143,56 @@ app.post('/urls', (req, res) => {
 
 //Add login cookie
 app.post("/login", (req, res) => {
-  res.cookie("username", req.body.username);
-  res.redirect("/urls");
+  let matchingUser = filterByAttribute(users, "email", req.body.email);
+
+  if(!matchingUser.length){
+    res.status(403).send({ error: "Invalid email" });
+  }
+  else if(matchingUser[0].password !== req.body.password) {
+    res.status(403).send({ error: "Invalid password" });
+  }
+  else{
+    res.cookie("user_id", matchingUser[0].id);
+    res.redirect("/");
+  }
 });
 
 //Add logout and clear cookie
 app.post("/logout", (req, res) => {
-  res.clearCookie("username");
+  res.clearCookie("user_id");
   res.redirect("/urls");
+});
+
+app.post("/register", (req, res) => {
+  if(!req.body.email || !req.body.password){
+    res.status(400).send({ error: "Invalid username or password" });
+  }
+
+  let emailMatches = filterByAttribute(users, "email", req.body.email);
+
+  if(emailMatches.length) {
+    res.status(400).send({ error: "Email already registered." });
+  } else {
+    let id = generateRandomString();
+    users[id] = {id: id, email: req.body.email, password: req.body.password};
+    res.cookie("user_id", id);
+    res.redirect("/urls");
+  }
 });
 
 app.post("/urls/:shortUrl/delete", (req, res) => {
   delete urlDatabase[req.params.shortUrl];
-  res.redirect("/urls", {username: req.cookies["username"]});
+  res.redirect("/urls", {user: users[req.cookies["user_id"]]});
 });
 
 app.post("/urls/:shortUrl", (req, res) => {
   urlDatabase[req.params.shortUrl] = req.body.longUrl;
-  res.redirect("/urls", {username: req.cookies["username"]});
+  res.redirect("/urls", {user: users[req.cookies["user_id"]]});
 });
+
+//#############################################
+//~~~~~~~~~~~~~~~~~~ LISTEN ~~~~~~~~~~~~~~~~~~~~~
+//#############################################
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
